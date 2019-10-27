47f31714f624f301bc105106f36b545e93c0b05a
Heikki Pora <heikki.pora@gmail.com>
2019-10-27 11:46:16 +0200

    Add unit test for image loading and saving
    Output image needs to be inspected visually

---
+++ test/image-unit.js
@@ -0,0 +1,19 @@
+import {expect} from 'chai'
+import {fileToPixels, pixelsToFile} from '../image.js'
+import fs from 'fs'
+
+const INPUT_FILE = `${__dirname}/../example-small.jpg`
+const OUTPUT_FILE = `${__dirname}/../example-small-test.jpg`
+
+describe('Image loading and saving', () => {
+  it('Should load an image split to three channels, and merge those back when saving', async () => {
+    const {r, g, b, width, height} = await fileToPixels(INPUT_FILE)
+    expect(width).to.equal(256)
+    expect(height).to.equal(256)
+    expect(r).to.have.lengthOf(256 * 256)
+    expect(g).to.have.lengthOf(256 * 256)
+    expect(b).to.have.lengthOf(256 * 256)
+    await pixelsToFile(r, g, b, width, height, OUTPUT_FILE)
+    await fs.promises.unlink(OUTPUT_FILE)
+  })
+})
