bcaff908f1aee72b3448a094d470c72d231c0e94
Heikki Pora <heikki.pora@gmail.com>
2019-10-27 12:59:42 +0200

    Add safeguards to block value writing

--- file-write.js
+++ file-write.js
@@ -33,6 +33,19 @@ function componentHeader(id, length) {
 }
 
 function block({t, o, x, y}) {
+  if (x >= 4096) {
+    throw Error(`X value ${x}s overflows 12 bits`)
+  }
+  if (y >= 4096) {
+    throw Error(`Y value ${y}s overflows 12 bits`)
+  }
+  if (t >= 32) {
+    throw Error(`T value ${t}s overflows 5 bits`)
+  }
+  if (o >= 4) {
+    throw Error(`O value ${t}s overflows 2 bits`)
+  }
+
   const buf = Buffer.alloc(4)
   const packed = o << 29 | t << 24 | x << 12 | y
   buf.writeUInt32LE(packed, 0)
